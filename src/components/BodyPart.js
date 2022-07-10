6c69f4fae4d0f4ceaa2116044866bbfa1898ffb5
vaganast <71605687+vaganast@users.noreply.github.com>
2022-07-10 23:00:11 +0300

    Schrollbar
    states at Home

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material'
 
-function BodyPart() {
+import Icon from '../assets/icons/gym.png'
+
+function BodyPart({ item, setBodyPart, bodyPart}) {
   return (
-    <div>BodyPart</div>
+    <Stack
+     type='button'
+     alignItems='center'
+     justifyContent='center'
+     className='bodyPart-card'
+     sx={{
+       borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+       backgroundColor: '#fff',
+       borderBottomLeftRadius: '20px',
+       width: '270px',
+       height: '280px',
+       cursor: 'pointer',
+       gap: '47px'
+    }}
+    onClick={() => {
+        setBodyPart(item)
+        window.schrollTo({top: 1800, left: 100, behavior: 'smooth'})
+    }}
+    >
+        <img src={Icon} alt='dumbbell' style={{ width: '40px', height: '40px'}} />
+        <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
+    </Stack>
   )
 }
 
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,9 +5,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchData'
 import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState(' ')
-  const [exercises, setExercises] = useState([])
   const [bodyParts, setBodyParts] = useState([])
 
   useEffect(() => {
@@ -78,7 +77,7 @@ const SearchExercises = () => {
           </Button>
         </Box>
         <Box sx={{ position:'relative', width:'100%', p:'20px'}}>
-            <HorizontalScrollbar data={bodyParts}  />
+            <HorizontalScrollbar data={bodyParts}  bodyPart={bodyPart} setBodyPart={setBodyPart}/>
         </Box>
     </Stack>
   )
